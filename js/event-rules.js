9b7230df7d2171895ff802910a39ea8dd269e419
bhavikj06 <bhavikj06@users.noreply.github.com>
2021-03-24 20:01:32 +0530

    immix18 is made by bhavik jain

---
+++ js/event-rules.js
@@ -0,0 +1,114 @@
+
+var pubg = document.getElementById("PUBG");
+var mm = document.getElementById("MM");
+var botb = document.getElementById("BOTB");
+var rdin = document.getElementById("RDIN");
+var pw = document.getElementById("PW");
+var bob = document.getElementById("BOB");
+var ss = document.getElementById("SS");
+var fof = document.getElementById("FOF");
+var mmi = document.getElementById("MMI");
+var dw = document.getElementById("DW");
+var fp = document.getElementById("FP");
+var m = document.getElementById("M");
+var sd = document.getElementById("SD");
+var coc = document.getElementById("COC");
+var bb = document.getElementById("BB");
+var r = document.getElementById("R");
+var k = document.getElementById("K");
+var a = document.getElementById("A");
+
+function O_PUBG() {
+    pubg.style.display = "block";
+}
+
+function O_MM() {
+    mm.style.display = "block";
+}
+
+function O_BOTB() {
+    botb.style.display = "block";
+}
+
+function O_RDIN() {
+    rdin.style.display = "block";
+}
+
+function O_PW() {
+    pw.style.display = "block";
+}
+
+function O_BOB() {
+    bob.style.display = "block";
+}
+
+function O_SS() {
+    ss.style.display = "block";
+}
+
+function O_FOF() {
+    fof.style.display = "block";
+}
+
+function O_MMI() {
+    mmi.style.display = "block";
+}
+
+function O_DW() {
+    dw.style.display = "block";
+}
+
+function O_FP() {
+    fp.style.display = "block";
+}
+
+function O_M() {
+    m.style.display = "block";
+}
+
+function O_SD() {
+    sd.style.display = "block";
+}
+
+function O_COC() {
+    coc.style.display = "block";
+}
+function O_BB() {
+    bb.style.display = "block";
+}
+
+function O_R() {
+    r.style.display = "block";
+}
+
+function O_K() {
+    k.style.display = "block";
+}
+
+function O_A() {
+    a.style.display = "block";
+}
+
+
+
+function C_ALL() {
+    pubg.style.display = "none";
+    mm.style.display = "none";
+    botb.style.display = "none";
+    rdin.style.display = "none";
+    pw.style.display = "none";
+    bob.style.display = "none";
+    ss.style.display = "none";
+    fof.style.display = "none";
+    mmi.style.display = "none";
+    dw.style.display = "none";
+    fp.style.display = "none";
+    m.style.display = "none";
+    sd.style.display = "none";
+    coc.style.display = "none";
+    bb.style.display = "none";
+    r.style.display = "none";
+    k.style.display = "none";
+    a.style.display = "none";
+
+}
